bda21cdd03a67f702e054b966881f86c3b835bf5
armanmk22-p <armanmk.programming22@gmail.com>
2023-05-17 06:17:49 -0700

    start ecommerce project

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:ecommerce_app/constants/app_colors.dart';
+import 'package:ecommerce_app/widgets/banner_slider.dart';
+import 'package:ecommerce_app/widgets/product_items.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,26 +12,223 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:Scaffold(
-        body: Column(
-          children: [
-              SizedBox(
-                height: 200,
-                child: PageView.builder(
-                    itemCount:3,
-                    itemBuilder:((context,index){
-                      return  Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Container(color: Colors.red,height: 200,),
-                      );
-                    })),
-              )
-          ],
+      home: Scaffold(
+        backgroundColor: AppColors.backgroundScaffoldColor,
+        body: SafeArea(
+          child: Center(
+            child: CustomScrollView(
+              slivers: [
+                SliverToBoxAdapter(
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 44, right: 44, bottom: 32,top: 20),
+                    child: Container(
+                      height: 46,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(15),
+                        ),
+                      ),
+                      child: Row(
+                        children: [
+                          SizedBox(width: 16,),
+                          Image.asset('assets/images/icon_search.png'),
+                          SizedBox(width: 10,),
+                          Expanded(
+                              child: Text(
+                            'search products',
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              fontStyle: FontStyle.italic,
+                              fontSize: 16,
+                              color: AppColors.grey,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )),
+                          Image.asset('assets/images/icon_apple_blue.png'),
+                          SizedBox(width: 16,),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SliverToBoxAdapter(
+                  child: BannerSlider(),
+                ),
+                SliverToBoxAdapter(
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.only(left: 44, right: 44, bottom: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          'Category',
+                          style: TextStyle(
+                              color: AppColors.grey,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SliverToBoxAdapter(
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 44),
+                    child: SizedBox(
+                      height: 100,
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 10,
+                          itemBuilder: ((context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.only(left: 20),
+                              child: CategoryHorizontalItemList(),
+                            );
+                          })),
+                    ),
+                  ),
+                ),
+                SliverToBoxAdapter(
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.only(left: 44, right: 44, bottom: 20),
+                    child: Row(
+                      children: [
+                        Image.asset('assets/images/icon_left_categroy.png'),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'See All',
+                          style: TextStyle(
+                              color: AppColors.blue,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Spacer(),
+                        Text(
+                          'Best Selling',
+                          style: TextStyle(
+                              color: AppColors.grey,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SliverToBoxAdapter(
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 44),
+                    child: SizedBox(
+                      height: 200,
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 10,
+                          itemBuilder: ((context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.only(left: 20),
+                              child: ProductItems(),
+                            );
+                          })),
+                    ),
+                  ),
+                ),
+                SliverToBoxAdapter(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                        left: 44, right: 44, bottom: 20, top: 32),
+                    child: Row(
+                      children: [
+                        Image.asset('assets/images/icon_left_categroy.png'),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'See All',
+                          style: TextStyle(
+                              color: AppColors.blue,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Spacer(),
+                        Text(
+                          'The Most Visited',
+                          style: TextStyle(
+                              color: AppColors.grey,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SliverToBoxAdapter(
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 44),
+                    child: SizedBox(
+                      height: 200,
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 10,
+                          itemBuilder: ((context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.only(left: 20),
+                              child: ProductItems(),
+                            );
+                          })),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
   }
 }
 
+class CategoryHorizontalItemList extends StatelessWidget {
+  const CategoryHorizontalItemList({
+    super.key,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Stack(
+          alignment: AlignmentDirectional.center,
+          children: [
+            Container(
+              height: 56,
+              width: 56,
+              decoration: ShapeDecoration(
+                  color: Colors.red,
+                  shadows: [
+                    BoxShadow(
+                        color: Colors.red,
+                        blurRadius: 25,
+                        spreadRadius: -12,
+                        offset: Offset(0.0, 15))
+                  ],
+                  shape: ContinuousRectangleBorder(
+                    borderRadius: BorderRadius.circular(40),
+                  )),
+            ),
+            Icon(
+              Icons.add,
+              size: 32,
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Text(
+          'Test',
+          style: TextStyle(fontFamily: 'SB', fontSize: 12),
+        ),
+      ],
+    );
+  }
+}

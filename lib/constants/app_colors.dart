64657612412e136fc69ac1af63184d1c26aa4386
armanmk22-p <armanmk.programming22@gmail.com>
2023-05-17 06:19:57 -0700

    start ecommerce project

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color blueIndicator = Color(0Xff3B5EDF);
+  static const Color backgroundScaffoldColor = Color(0XffEEEEEE);
+  static const Color blue = Color(0xff3B5EDF);
+  static const Color red = Color(0xffD02026);
+  static const Color green = Color(0xff1DB68B);
+  static const Color grey = Color(0xff858585);
+}
